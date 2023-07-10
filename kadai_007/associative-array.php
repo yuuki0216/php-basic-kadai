c333b0b3ee9a52accdd75887bba547b1a94c224e
ume <130993943+yuuki0216@users.noreply.github.com>
2023-07-11 00:34:38 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP基礎編</title>
+ </head>
+ 
+ <body>
+     <p>
+        <?php
+        $deta = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+        print_r($deta);
+        ?>
+     </p>
+
+</body>
+
+</html>
